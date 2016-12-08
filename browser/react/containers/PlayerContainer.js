d83fd09ab0b2bfb79dc66b0648cf448b92b7846e
Jenny Chan <jrc337@cornell.edu>
2016-12-08 15:01:14 -0500

    refactoring player container

--- browser/react/containers/PlayerContainer.js
+++ browser/react/containers/PlayerContainer.js
@@ -1,54 +1,91 @@
-import React, {Component} from 'react';
+import { connect } from 'react-redux';
 import AUDIO from '../audio';
-import store from '../store';
 import {previous, next, setProgress, toggleSong} from '../action-creators/player';
 import Player from '../components/Player';
 
-export default class extends Component {
+const mapStateToProps = (state, ownProps) => {
+  return {
+    currentSong: state.player.currentSong,
+    currentSongList: state.player.currentSongList,
+    isPlaying: state.player.isPlaying,
+    progress: state.player.progress
+  };
+};
 
-  constructor() {
-    super();
-    this.state = store.getState().player;
-    this.toggle = this.toggle.bind(this);
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    next: function(){
+      dispatch(next())
+    },
+    prev: function() {
+      dispatch(previous())
+    },
+    toggleOne: function(song, list) {
+      dispatch(toggleSong(song, list));
+    },
+    setProgress: function() {
+      // AUDIO.addEventListener('ended', this.next);
+      AUDIO.addEventListener('timeupdate', () => {
+      dispatch(setProgress(AUDIO.currentTime / AUDIO.duration))
+    })
   }
+};
 
-  componentDidMount() {
+export default connect(mapStateToProps, mapDispatchToProps)(Player);
 
-    AUDIO.addEventListener('ended', this.next);
-    AUDIO.addEventListener('timeupdate', () => {
-      store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
-    });
 
-    this.unsubscribe = store.subscribe(() => {
-      this.setState(store.getState().player);
-    });
-  }
 
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
 
-  next() {
-    store.dispatch(next());
-  }
 
-  prev() {
-    store.dispatch(previous());
-  }
 
-  toggle() {
-    store.dispatch(
-      toggleSong(this.state.currentSong, this.state.currentSongList)
-    );
-  }
+// import React, {Component} from 'react';
+// import store from '../store';
 
-  render() {
-    return <Player
-      {...this.state}
-      next={this.next}
-      prev={this.prev}
-      toggle={this.toggle}
-    />;
-  }
+// export default class extends Component {
 
-}
+//   constructor() {
+//     super();
+//     this.state = store.getState().player;
+//     this.toggle = this.toggle.bind(this);
+//   }
+
+//   componentDidMount() {
+
+    // AUDIO.addEventListener('ended', this.next);
+    // AUDIO.addEventListener('timeupdate', () => {
+    //   store.dispatch(setProgress(AUDIO.currentTime / AUDIO.duration));
+    // });
+
+//     // this.unsubscribe = store.subscribe(() => {
+//     //   this.setState(store.getState().player);
+//     // });
+//   }
+
+//   // componentWillUnmount() {
+//   //   this.unsubscribe();
+//   // }
+
+//   next() {
+//     store.dispatch(next());
+//   }
+
+//   prev() {
+//     store.dispatch(previous());
+//   }
+
+//   toggle() {
+//     store.dispatch(
+//       toggleSong(this.state.currentSong, this.state.currentSongList)
+//     );
+//   }
+
+//   render() {
+//     return <Player
+//       {...this.state}
+//       next={this.next}
+//       prev={this.prev}
+//       toggle={this.toggle}
+//     />;
+//   }
+
+// }
